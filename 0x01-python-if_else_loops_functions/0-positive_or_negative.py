32b94f9e73d8366bedf1a685a614abba13bb460d
Peacebern <peacebern@gmail.com>
2022-05-31 12:24:15 -0700

    [fix]: replacing elif statements with just if statements

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10, 10)
 if number > 0:
     print(f"is positive\n")
-elif number == 0:
+if number == 0:
     print(f"is zero\n")
-elif number < 0:
+if number < 0:
     print(f"is negative\n")
