56fdad99b9d5bd7fdaacb35d9346c76a6d4b1f14
Peacebern <peacebern@gmail.com>
2022-06-05 12:07:08 -0700

    [fix: replaced the condition statement and changed the local variable opert to global variable

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
+if __name__ == '__main__':
+    from calculator_1 import add, sub, div, mul
+
     import sys
 
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    opert = {"+": add, "-": sub, "*": mul, "/": div}
+
+    if len(sys.argv) -1 != 3:
+        print("usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+        # create an array of various operators
+
+       # opert = {"+": add, "-": sub, "*": mul, "/": div}
+
+    elif sys.argv[2] not in list(opert.keys()):
+        print("unknown operator. Available operators: +, -, *, and /")
         sys.exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        print("{} {} {} = {}".format(a, sys.argv[2], b, opert[sys.argv[2]](a, b)))
